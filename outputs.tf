bd1c1ad7cf4313f04da0a801235aec0b5e201248
dani ignatious <daniignatious@gmail.com>
2026-03-01 00:12:30 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+output "public_ip" {
+  value = azurerm_public_ip.pip.ip_address
 }
 
-output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+output "vm_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "ssh_command" {
+  value = "ssh ${var.admin_username}@${azurerm_public_ip.pip.ip_address}"
 }
