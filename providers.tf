2223c4e27edea77b6e06207abd68c24a80aabe28
dani ignatious <daniignatious@gmail.com>
2026-03-01 00:06:37 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,18 +1,10 @@
 terraform {
-  required_version = ">=0.12"
+  required_version = ">= 1.5.0"
 
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "~>1.5"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "~> 3.100"
     }
   }
 }
