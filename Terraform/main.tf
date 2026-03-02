0ce5ef9f626898f59d89c72fe3b36573a1fc49bf
dani ignatious <daniignatious@gmail.com>
2026-03-03 00:21:51 +0100

    main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,125 @@
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# ----------------------------
+# Resource Group
+# ----------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "demo-vm-rg"
+  location = "East US"
+}
+
+# ----------------------------
+# Virtual Network
+# ----------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# ----------------------------
+# Subnet
+# ----------------------------
+resource "azurerm_subnet" "subnet" {
+  name                 = "demo-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# ----------------------------
+# Public IP
+# ----------------------------
+resource "azurerm_public_ip" "public_ip" {
+  name                = "demo-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# ----------------------------
+# Network Security Group
+# ----------------------------
+resource "azurerm_network_security_group" "nsg" {
+  name                = "demo-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# ----------------------------
+# Network Interface
+# ----------------------------
+resource "azurerm_network_interface" "nic" {
+  name                = "demo-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Associate NSG to NIC
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# ----------------------------
+# Linux Virtual Machine
+# ----------------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "demo-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_password                  = "Password1234!"
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
