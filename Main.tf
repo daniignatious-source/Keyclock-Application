5ccc5578091e67fbba2cc9bc66b5fe364bf0f4ce
dani ignatious <daniignatious@gmail.com>
2026-03-03 00:42:14 +0100

    Create Main.tf

---
+++ Main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Variables inside same file
+variable "resource_group_name" {
+  default = "rg-github-cicd-test"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "storage_account_name" {
+  default = "githubcicdstorage12345" # must be globally unique
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Storage Account
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
