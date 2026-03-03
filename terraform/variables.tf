95664fc5959ff459ef54da05fb7714d7f3c01d42
dani ignatious <daniignatious@gmail.com>
2026-03-03 01:31:03 +0100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "location" { default = "westeurope" }
+variable "resource_group_name" { default = "rg-tf-vm" }
+variable "vm_name" { default = "tf-ubuntu-vm" }
+variable "admin_username" { default = "azureuser" }
+
+# Put your PUBLIC ssh key here (not private key)
+variable "ssh_public_key" {
+  type = string
+}
