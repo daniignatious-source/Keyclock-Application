c015d3cd77924089afb04dc9d77dc027831bb220
dani ignatious <daniignatious@gmail.com>
2026-03-05 01:42:31 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_public_ip" "pip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  sku                 = "Standard"
+  sku                 = "Standard_B1s"
 }
 
 resource "azurerm_network_interface" "nic" {
